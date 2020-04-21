da5c0fcd153bc38d356a37c6180956a5dbdc38ef
Luis Gustavo Santos <gustavoluisrh@gmail.com>
2020-04-21 01:12:41 -0300

    Limpando cpf

--- index.js
+++ index.js
@@ -13,7 +13,23 @@ function ValidaCPF(cpfEnviado) {
 ValidaCPF.prototype.valida = function() {
     if(typeof this.cpfLimpo === 'undefined') return false;
     if(this.cpfLimpo.length !== 11) return false;
+
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    let digito1 = this.criaDigito(cpfParcial);
+
     return true;
+}
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+    }, 0);
+    const digito = 11 - (total % 11);
+    return digito > 9 ? 0 : digito;
 }
 
 const cpf = new ValidaCPF('705.484.450-52');
